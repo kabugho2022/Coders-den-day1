ff07543e95818ca4056b998a1e4fe7aed7232f9d
Judith Kabugho <109104678+kabugho2022@users.noreply.github.com>
2022-10-16 00:25:37 +0300

    Add files via upload

---
+++ day.js
@@ -0,0 +1,28 @@
+let challenge='30 Days Of Javascript'
+console.log(challenge);
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(0, 1))
+console.log(challenge.slice(3))
+console.log(challenge.includes("string"))
+console.log(challenge.split(""))
+console.log(challenge.split(" "))
+let softwareCompanies="Facebook,Google,Microsoft,Apple,IBM,Oracle,Amazon"
+console.log(softwareCompanies.split(","))
+let challenge2='30 Days of python'
+console.log(challenge2.replace("javascript", "python"));
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt("j"))
+console.log(challenge.indexOf("a"))
+let message="You cannot end a sentence  with because because because is a conjuction"
+console.log(message.indexOf("because"))
+console.log(message.lastIndexOf(""))
+console.log(message.search("You"))
+console.log(challenge.trim())
+console.log(challenge.startsWith(''))
+console.log(challenge.endsWith(''))
+console.log(challenge.match("a"))
+console.log(challenge.concat(''))
+console.log(challenge.repeat("challenge"))
+
